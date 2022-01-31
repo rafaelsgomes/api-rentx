132b3667ce57c46defb85a748ac4660ce78a741e
Rafael Gomes <rafaelsgomes.dev@gmail.com>
2022-01-31 17:32:36 -0300

    Cors Adjustments

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -24,7 +24,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
-app.user(cors());
+app.use(cors());
 
 app.use(router);
 
